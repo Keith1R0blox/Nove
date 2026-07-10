501cec1276057c2deb41102216d6b507292a67d5
Keith1 <155455657+Keith1R0blox@users.noreply.github.com>
2026-07-10 11:29:57 +0700

    user theme update

--- style.js
+++ style.js
@@ -4,7 +4,28 @@ let csss = document.getElementById("csss")
 
 window.addEventListener("DOMContentLoaded", () => {
     let csss = document.getElementById("csss")
-    if (csstype && csss) {
-        csss.setAttribute("href", "../../" + csstype)
-    }
+    if (!csss || !csstype) return;
+        if (csstype.startsWith("http")) {
+            csss.href = csstype;
+        } else {
+            csss.href = "../../" + csstype
+        }
 })
+
+    ///user theme apply
+    async function CSSuserapply(url) {
+        const responce = await fetch(url)
+        const cssText = await responce.text();
+        let styletag = document.getElementById("remote-theme")
+        if(!styletag) {
+            styletag = document.createElement("style")
+            styletag.id = "remote-theme"
+            document.head.appendChild(styletag)
+        }
+        styletag.textContent = cssText
+    }
+
+        const savedStyle = localStorage.getItem("style");
+        if (savedStyle && savedStyle.startsWith("http")) {
+            CSSuserapply(savedStyle)
+        }
